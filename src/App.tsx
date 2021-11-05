6cb3e836b68078ae0f31fb4796973d231fc13bdd
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2021-11-05 10:17:38 -0300

    Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1> Helow Juninho</h1>
-
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
