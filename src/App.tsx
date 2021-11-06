2530b327d533fdcc93b56e6e47996f9f31039d37
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2021-11-06 11:12:38 -0300

    Configurando modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dasboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
       <Dasboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
