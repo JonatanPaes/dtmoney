bd46a6dba26b326c25c91ca4bef58367b0451d4c
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2021-11-08 12:35:51 -0300

    Calculando resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,6 +8,21 @@ import { Container } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const sumary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposit += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    { deposit: 0, withdraws: 0, total: 0 }
+  );
+
   return (
     <Container>
       <div>
@@ -15,21 +30,37 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"></img>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(sumary.deposit)}
+        </strong>
       </div>
       <div>
         <header>
-          <p>Entradas</p>
+          <p>Saídas</p>
           <img src={outcomeImg} alt="Saidas"></img>
         </header>
-        <strong>- R$5000,00</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(sumary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
-          <p>Entradas</p>
+          <p>Total</p>
           <img src={totalcomeImg} alt="Total"></img>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(sumary.total)}
+        </strong>
       </div>
     </Container>
   );
